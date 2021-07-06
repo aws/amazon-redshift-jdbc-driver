44da6ca7150435db6f99b3e7cb835841f2385701
ilesh Garish <iggarish@amazon.com>
2021-07-06 20:04:02 +0000

    Return current database instead of NULL for getSchemas() API

--- src/main/java/com/amazon/redshift/jdbc/RedshiftDatabaseMetaData.java
+++ src/main/java/com/amazon/redshift/jdbc/RedshiftDatabaseMetaData.java
@@ -1884,7 +1884,7 @@ public class RedshiftDatabaseMetaData implements DatabaseMetaData {
     	connection.getLogger().logFunction(true, catalog, schemaPattern);
     
 	  if (isSingleDatabaseMetaData()) {
-	    sql = "SELECT nspname AS TABLE_SCHEM, NULL AS TABLE_CATALOG FROM pg_catalog.pg_namespace "
+	    sql = "SELECT nspname AS TABLE_SCHEM, current_database() AS TABLE_CATALOG FROM pg_catalog.pg_namespace "
 	          + " WHERE nspname <> 'pg_toast' AND (nspname !~ '^pg_temp_' "
 	          + " OR nspname = (pg_catalog.current_schemas(true))[1]) AND (nspname !~ '^pg_toast_temp_' "
 	          + " OR nspname = replace((pg_catalog.current_schemas(true))[1], 'pg_temp_', 'pg_toast_temp_')) ";
