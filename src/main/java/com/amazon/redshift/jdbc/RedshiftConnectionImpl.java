d4c11bcffbe45e105ec8eb8f4a963c03182f783a
premgk <prgk@lunar.app>
2025-09-12 08:18:52 +0200

    added OktaRedshiftPlugin to the RedshiftConnectionImpl.java

--- src/main/java/com/amazon/redshift/jdbc/RedshiftConnectionImpl.java
+++ src/main/java/com/amazon/redshift/jdbc/RedshiftConnectionImpl.java
@@ -197,7 +197,8 @@ public class RedshiftConnectionImpl implements BaseConnection {
           RedshiftConstants.NATIVE_IDP_AZUREAD_BROWSER_PLUGIN,
           RedshiftConstants.NATIVE_IDP_OKTA_BROWSER_PLUGIN,
           RedshiftConstants.IDP_TOKEN_PLUGIN,
-          RedshiftConstants.IDC_PKCE_BROWSER_PLUGIN));
+          RedshiftConstants.IDC_PKCE_BROWSER_PLUGIN,
+          RedshiftConstants.IDC_PKCE_BROWSER_OKTA_PLUGIN));
 
   final CachedQuery borrowQuery(String sql) throws SQLException {
     return queryExecutor.borrowQuery(sql);
@@ -264,7 +265,8 @@ public class RedshiftConnectionImpl implements BaseConnection {
             RedshiftProperty.CREDENTIALS_PROVIDER.getName(), info);
         if(iamCredentialProvider != null &&
           (iamCredentialProvider.equalsIgnoreCase(RedshiftConstants.IDP_TOKEN_PLUGIN) ||
-           iamCredentialProvider.equalsIgnoreCase(RedshiftConstants.IDC_PKCE_BROWSER_PLUGIN))) {
+           iamCredentialProvider.equalsIgnoreCase(RedshiftConstants.IDC_PKCE_BROWSER_PLUGIN) ||
+           iamCredentialProvider.equalsIgnoreCase(RedshiftConstants.IDC_PKCE_BROWSER_OKTA_PLUGIN))) {
             throw new RedshiftException(GT.tr("You can not use this authentication plugin with IAM enabled."),
                 RedshiftState.UNEXPECTED_ERROR);
         }
